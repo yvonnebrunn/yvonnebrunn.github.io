13dc284a34dbe230d43dd8c4721ecd381a428c30
yvonnebrunn <yvonne.brunnas@gmail.com>
2026-04-13 00:04:22 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,9 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
+export default 
+  defineConfig(({ mode }) => ({
+    **base: '/Begleitung/',**
   server: {
     host: "::",
     port: 8080,
